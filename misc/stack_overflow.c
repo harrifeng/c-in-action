d6438eb3485297f3102ad293ed939faea8a74b51
Haoran Feng <haoran.feng@sap.com>
2015-08-16 23:41:57 -0700

    new overflow

---
+++ misc/stack_overflow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void stack_overflow(int depth) {
+    char blockdata[1024];
+    printf("Overflow: %d\n", depth);
+    stack_overflow(depth + 1);
+}
+
+
+int main(int argc, char *argv[])
+{
+    stack_overflow(0);
+    return 0;
+}
