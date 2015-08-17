2d242caea630021513bf830214cf84536bd4897e
Haoran Feng <haoran.feng@sap.com>
2015-08-16 23:46:14 -0700

    1mb stack

--- misc/stack_overflow.c
+++ misc/stack_overflow.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void stack_overflow(int depth) {
-    char blockdata[1024];
+    char blockdata[1024 * 1024];
     printf("Overflow: %d\n", depth);
     stack_overflow(depth + 1);
 }
