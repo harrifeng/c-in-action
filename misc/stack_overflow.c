e59f21544a4d439c87d091481a7a9408a077962d
Haoran Feng <haoran.feng@sap.com>
2015-08-17 14:58:43 +0800

    stack size is only about 8MB on MAC!!!

--- misc/stack_overflow.c
+++ misc/stack_overflow.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 void stack_overflow(int depth) {
-    char blockdata[1024 * 1024];
+    /* Usually gcc us 32 bytes for one call */
+    char blockdata[1024 -32];
     printf("Overflow: %d\n", depth);
     stack_overflow(depth + 1);
 }
@@ -12,3 +13,23 @@ int main(int argc, char *argv[])
     stack_overflow(0);
     return 0;
 }
+
+/*********************************************************/
+/* <===================OUTPUT On Mac===================> */
+/* i309511@ misc (master) $ ulimit -a                    */
+/* core file size          (blocks, -c) 0                */
+/* data seg size           (kbytes, -d) unlimited        */
+/* file size               (blocks, -f) unlimited        */
+/* max locked memory       (kbytes, -l) unlimited        */
+/* max memory size         (kbytes, -m) unlimited        */
+/* open files                      (-n) 256              */
+/* pipe size            (512 bytes, -p) 1                */
+/* stack size              (kbytes, -s) 8515             */
+/* cpu time               (seconds, -t) unlimited        */
+/* max user processes              (-u) 709              */
+/* virtual memory          (kbytes, -v) unlimited        */
+/* i309511@ misc (master) $ ./a.out                      */
+/* Overflow: 8378                                        */
+/* Overflow: 8379                                        */
+/* Segmentation fault: 11                                */
+/*********************************************************/
